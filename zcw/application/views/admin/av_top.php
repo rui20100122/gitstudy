8ad9979e025dc9c0e9c7d88876db2cebc8e160b8
zcw000505 <32184180@qq.com>
2018-03-27 13:32:46 +0800

    delete file name "zcw"

--- zcw/application/views/admin/av_top.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
-	<title>top</title>
-</head>
-<body style="background:#666666;">
-</body>
-</html>
